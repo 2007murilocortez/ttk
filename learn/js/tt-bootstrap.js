e766c7f83cec96c2dd1ed9e40901b749d4c86dbd
Murilo Cortez <musgroi@uol.com.br>
2026-09-24 10:37:02 -0300

    Corrige pixel TikTok: consentimento GDPR e eventos apos ttq.ready.

---
+++ learn/js/tt-bootstrap.js
@@ -0,0 +1,63 @@
+(function (w, d) {
+  "use strict";
+
+  var PIXEL_ID = "DAQHTRRC77U88MSO4JKG";
+
+  !function (win, doc, t) {
+    win.TiktokAnalyticsObject = t;
+    var ttq = (win[t] = win[t] || []);
+    ttq.methods = [
+      "page", "track", "identify", "instances", "debug", "on", "off", "once",
+      "ready", "alias", "group", "enableCookie", "disableCookie",
+      "holdConsent", "revokeConsent", "grantConsent",
+    ];
+    ttq.setAndDefer = function (obj, method) {
+      obj[method] = function () {
+        obj.push([method].concat(Array.prototype.slice.call(arguments, 0)));
+      };
+    };
+    for (var i = 0; i < ttq.methods.length; i++) {
+      ttq.setAndDefer(ttq, ttq.methods[i]);
+    }
+    ttq.instance = function (id) {
+      var inst = ttq._i[id] || [];
+      for (var n = 0; n < ttq.methods.length; n++) {
+        ttq.setAndDefer(inst, ttq.methods[n]);
+      }
+      return inst;
+    };
+    ttq.load = function (id, opts) {
+      var src = "https://analytics.tiktok.com/i18n/pixel/events.js";
+      ttq._i = ttq._i || {};
+      ttq._i[id] = [];
+      ttq._i[id]._u = src;
+      ttq._t = ttq._t || {};
+      ttq._t[id] = +new Date();
+      ttq._o = ttq._o || {};
+      ttq._o[id] = opts || {};
+      var s = doc.createElement("script");
+      s.type = "text/javascript";
+      s.async = true;
+      s.src = src + "?sdkid=" + id + "&lib=" + t;
+      var first = doc.getElementsByTagName("script")[0];
+      first.parentNode.insertBefore(s, first);
+    };
+  }(w, d, "ttq");
+
+  ttq.load(PIXEL_ID);
+  ttq.grantConsent();
+  ttq.enableCookie();
+  ttq.page();
+
+  ttq.ready(function () {
+    var path = w.location.pathname || "";
+    var isProductPage =
+      path.indexOf("/learn/front") !== -1 ||
+      path.indexOf("/learn/confirmar-saque") !== -1 ||
+      path.indexOf("/learn/pre") !== -1;
+
+    if (isProductPage && w.ttPixel && typeof w.ttPixel.viewContent === "function") {
+      w.ttPixel.viewContent();
+    }
+  });
+})(window, document);
